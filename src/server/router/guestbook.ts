e51c6c67526dfcf5926fce53b1caf1c8ba8712bc
Ryan Olsen <ryankolsen@gmail.com>
2022-09-05 12:43:38 -0400

    feat: can edit posts

--- src/server/router/guestbook.ts
+++ src/server/router/guestbook.ts
@@ -10,10 +10,34 @@ export const guestBookRouter = createRouter()
           select: {
             name: true,
             message: true,
+            id: true,
           },
         });
       } catch (error) {
         console.log("Error", error);
+      }
+    },
+  })
+
+  .query("getOne", {
+    input: z.object({
+      messageId: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      const { messageId } = input;
+      try {
+        return await ctx.prisma.guestbook.findFirst({
+          select: {
+            name: true,
+            message: true,
+            id: true,
+          },
+          where: {
+            id: messageId,
+          },
+        });
+      } catch (error) {
+        console.error(`Error getOne ${error}`);
       }
     },
   })
@@ -39,6 +63,26 @@ export const guestBookRouter = createRouter()
         });
       } catch (error) {
         console.log(`Error : ${error}`);
+      }
+    },
+  })
+  .mutation("editPost", {
+    input: z.object({
+      message: z.string(),
+      id: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      try {
+        await ctx.prisma.guestbook.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            message: input.message,
+          },
+        });
+      } catch (error) {
+        console.log(`editPost Error: ${error}`);
       }
     },
   });
