40aaa659b90b20c32ba373c52fdfeae7d1d3396a
Ryan Olsen <ryankolsen@gmail.com>
2022-09-08 19:12:33 -0400

    style:  edit post is fixed

--- src/components/editMessage.tsx
+++ src/components/editMessage.tsx
@@ -1,7 +1,7 @@
 import { trpc } from "../utils/trpc";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type FormInputs = {
   messageContent: string;
@@ -20,13 +20,9 @@ const EditMessage = ({ messageId, setMessageId }: ComponentInputs) => {
     register,
     handleSubmit,
     reset,
-    setFocus,
-    formState: { errors },
+    getValues,
+    formState: { isDirty, errors },
   } = useForm<FormInputs>();
-
-  useEffect(() => {
-    setFocus("messageContent", { shouldSelect: true });
-  }, []);
 
   const postEdit = trpc.useMutation("guestbookeditPost", {
     onMutate: () => {
@@ -63,15 +59,16 @@ const EditMessage = ({ messageId, setMessageId }: ComponentInputs) => {
 
   const ctx = trpc.useContext();
   const { data: session } = useSession();
+  const values = getValues;
 
   if (isLoading) return <div>Fetching Messages</div>;
 
   return (
-    <div>
+    <div className="fixed top-40 bg-black p-5 rounded-md border-zinc-700 border-2 ">
       <h1 className="py-2 text-xl">Edit This Post</h1>
       <form className="flex justify-around" onSubmit={handleSubmit(onSubmit)}>
         <textarea
-          className="px-4 py-2 w-96  border-violet-700 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none "
+          className="px-4 py-2 w-96  border-violet-700 rounded-md border-2  bg-neutral-900 focus:outline-none "
           {...register("messageContent", { required: true, maxLength: 100 })}
           placeholder={message?.message}
         />
@@ -81,7 +78,7 @@ const EditMessage = ({ messageId, setMessageId }: ComponentInputs) => {
         <div className="flex-col">
           <div className="p-2">
             <button
-              className=" sm:h-12 my-auto ml-5 p-2 bg-zinc-800 rounded-md border-2 border-violet-700 focus:outline-none "
+              className=" sm:h-12 my-auto ml-5 p-2 bg-zinc-800 rounded-md border-2 border-violet-700 focus:outline-none"
               type="submit"
             >
               Revise
